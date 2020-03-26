6c1c56d5606ab87ca16502421b279276ca8e543f
Jordan2811 <61702294+Jordan2811@users.noreply.github.com>
2020-03-26 18:17:34 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -4,6 +4,7 @@ $firstname =  $_GET["firstname"];
 $subject = $_GET["subject"];
 $mail = $_GET["mail"];
 $phone =$_GET["phone"];
+$message = $_GET["message"];
 echo "Merci $lastname $firstname  de nous avoir contacté à propos de $subject . </br>";
 echo "Un de nos conseiller vous contactera  $mail  ou par téléphone au $phone dans les plus brefs délais pour traiter votre demande: </br>";
-echo $phone;
+echo $message;
