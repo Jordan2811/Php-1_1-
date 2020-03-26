1a2ac704a4216e9a1290045e038317d4616cf169
Jordan2811 <61702294+Jordan2811@users.noreply.github.com>
2020-03-26 18:01:50 +0100

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,4 +1,4 @@
 <?php
-echo "Merci $_GET["lastname"] $_GET["firstname"] de nous avoir contacté à propos de $_GET[taskOption]. </br>";
+echo "Merci $_GET["lastname"] $_GET["firstname"] de nous avoir contacté à propos de $_GET["subject"]. </br>";
 echo "Un de nos conseiller vous contactera  $_GET["mail"] ou par téléphone au $_GET["phone"] dans les plus brefs délais pour traiter votre demande: </br>";
 echo "$_GET["message"]";
